93245e3b9549bd085b6cc3a9cc0a7b52ec428380
Shahin <amirmohammadkazemi2002@gmail.com>
2021-03-18 21:37:13 +0330

    Ground added

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 
+void ground(){
+    	int width=100; //arz
+    int length=20; //tool
+    //print top of ground
+    for(int k=0;k<width;k++){
+        cout<<"#";
+    }
+    cout<<endl;
+    //print left & right of ground
+    for(int i=0;i<length;i++){
+	    	for(int j=0;j<width;j++){
+	    		if(j==0 || j==width-1){
+    		    cout<<"#";
+			}
+			else{
+				cout<<" ";
+			}
+			}
+			cout<<endl;
+	}
+	//print bottom of ground
+	for(int k=0;k<width;k++){
+        cout<<"#";
+    }
+    
+}
+
 int main(){
+	ground();
     return 0;
 }
