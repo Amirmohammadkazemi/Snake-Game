0edec8dea22b705c628a1485eff92e47e4914538
Shahin <amirmohammadkazemi2002@gmail.com>
2021-03-19 15:35:16 +0330

    move function added

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
+int width=100; //arz
+int length=20; //tool
+int x=length/2,y=width/2;
 
+enum m_new{
+	stop=0,Left,Right,Up,Down
+};
+m_new dir;
+
+/* Ground function */
 void ground(){
-    	int width=100; //arz
-    int length=20; //tool
     //print top of ground
     for(int k=0;k<width;k++){
         cout<<"#";
@@ -14,6 +22,9 @@ void ground(){
 	    	for(int j=0;j<width;j++){
 	    		if(j==0 || j==width-1){
     		    cout<<"#";
+			}
+			if(i==x && j==y){
+				cout<<"O";
 			}
 			else{
 				cout<<" ";
@@ -28,7 +39,49 @@ void ground(){
     
 }
 
-int main(){
+void move(){
+	if(_kbhit()){
+		switch (_getch()){
+			case 'w':
+			dir=Up;
+			break;
+
+			case 's':
+			dir=Down;
+			break;
+
+			case 'a':
+			dir=Left;
+			break;
+
+			case 'd':
+			dir=Right;
+			break;
+		}
+	}
+switch (dir)
+{
+case Up:
+    y++;
+	break;
+
+case Down:
+    y--;
+	break;
+
+case Left:
+    x--;
+	break;
+	
+case Right:
+    x++;
+	break;			
+}	
+}
+
+/* Main function */
+int main(int argc, char** argv){
 	ground();
+	move();
     return 0;
 }
