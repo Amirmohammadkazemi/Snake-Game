0c65a7f938e18528dd4627e688877c9d84037909
AmirmohammadKazemi <amirmohammadkazemi2002@gmail.com>
2023-08-05 15:20:30 +0330

    move snake

--- cpp/snake-cpp.cpp
+++ cpp/snake-cpp.cpp
@@ -1,5 +1,8 @@
 //includes
 #include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include<unistd.h>   
 
 //namespaces
 using namespace std;
@@ -8,8 +11,19 @@ using namespace std;
 int w = 60; //width of map
 int l = 20; //length of map
 
+//The coordinates of the snake's head
+int x = w/2;
+int y = l/2;
+
+enum mNew {
+	stop=0, RIGHT, LEFT, UP, DOWN
+};
+
+mNew dir;
+
 //pulling the ground
 void map() {
+	system("clear"); //clear consol
 
 	//top of ground
 	for(int i=0; i<w; i++) {
@@ -20,8 +34,10 @@ void map() {
 	//right and left of ground
 	for(int i=0; i<l; i++) {
 		for(int j=0; j<w; j++) {
-			if(j==0 || j==w-1)
+			if(j==0 || j==w-2)
 				cout << "#";
+			if(i==y && j==x)
+				cout << "O";
 			else
 				cout << " ";
 		}
@@ -35,11 +51,56 @@ void map() {
 	cout << endl;
 }
 
+//move function
+void move() {
+	if(_kbhit()) {
+		switch(getch()){
+			case 'w':
+				dir = UP;
+				break;
+			case 's':
+				dir = DOWN;
+				break;
+			case 'a':
+				dir = LEFT;
+				break;
+			case 'd':
+				dir = RIGHT;
+				break;
+		}
+	}
+	switch(dir) {
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		default:
+			break;
+	}
+}
+
+//
+void Sleep(int speed) {
+	//sleep(speed);
+	usleep(100000);
+}
+
 //start function
 int main(int argc, char ** argv) {
 
-	map();
-
+	while(true) {
+		map();
+		move();
+		Sleep(1);
+	}
 
 	return 0;
 }
